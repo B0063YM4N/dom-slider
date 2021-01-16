5769c16a03d59cd332831327f22a6aac51ff42aa
gogatadze <gogatadzebeka@gmail.com>
2021-01-16 19:16:25 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const gallery = document.querySelector('#gallery')
+const images = document.querySelectorAll('#gallery .photos img')
+
+const prev = document.querySelector('#gallery .buttons .prev')
+const next = document.querySelector('#gallery .buttons .next')
+
+let i = 0 ;
+
+next.onclick=function(){
+console.log("next clicked")
+images[i].className='';
+i--; 
+if(i < 0){
+    i=5;
+}
+images[i].className='show';
+
+}
+
+prev.onclick=function(){
+    console.log("prev clicked")
+    images[i].className='';
+    i++; 
+    if(i === images.length){
+        i=0;
+    }
+    images[i].className='show';
+    
+    }
